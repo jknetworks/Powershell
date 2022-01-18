5d2636f32ab9c521e74d7a33466f10ebdbd1c190
John Kaiser <67724664+jknetworks@users.noreply.github.com>
2022-01-18 12:55:03 -0800

    Add files via upload

---
+++ GetProcesses.ps1
@@ -0,0 +1,5 @@
+﻿# PROCESSES
+
+Get-Process | Where-Object {$_.Name -eq "SVCHost" -and $_.PM -gt 5mb} | Sort-Object -Property PM -Descending
+
+Get-WmiObject Win32_Service | Where-Object {$_.ProcessID -eq <SVCHost_ID>}
