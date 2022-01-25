8486a2c6a76c8287d265b2580d7313341e63d8db
John Kaiser <67724664+jknetworks@users.noreply.github.com>
2022-01-25 14:30:26 -0800

    Add files via upload

---
+++ CreateLocalAccount.ps1
@@ -0,0 +1,6 @@
+﻿# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.localaccounts/new-localuser?view=powershell-5.1 
+
+# CREATE NEW LOCAL USER
+
+$Password = Read-Host -AsSecureString
+New-LocalUser "User" -Password $Password -FullName "First Last" -Description "Description of this account." -PasswordNeverExpires
