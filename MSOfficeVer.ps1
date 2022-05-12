1723f3b3ee6548933a2dc5e8f4940a6cbe291fa0
John Kaiser <67724664+jknetworks@users.noreply.github.com>
2022-05-12 09:44:10 -0700

    Add files via upload

---
+++ MSOfficeVer.ps1
@@ -0,0 +1,7 @@
+﻿foreach ($computer in (Get-Content "c:\psscripts\computers.txt")){
+  write-verbose "Working on $computer..." -Verbose
+  Invoke-Command -ComputerName "$Computer" -ScriptBlock {
+    Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\O365ProPlusRetail* |
+    Select-Object DisplayName, DisplayVersion, Publisher
+  } | export-csv C:\psscripts\results.csv -Append -NoTypeInformation
+}
