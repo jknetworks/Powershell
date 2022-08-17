aa09a787d40184029e8f656fd497bc95f7c4740c
John Kaiser <67724664+jknetworks@users.noreply.github.com>
2022-08-17 10:01:24 -0700

    Update 365FullMailboxPerms.ps1

--- 365FullMailboxPerms.ps1
+++ 365FullMailboxPerms.ps1
@@ -1,3 +1,8 @@
+#INSTALL EXCHANGEONLINEMANAGEMENT MODULE
+Find-Module -Name ExchangeOnlineManagement
+Install-Module -Name ExchangeOnlineManagement -Scope AllUsers
+Get-Command -Module ExchangeOnlineManagement
+
 Connect-ExchangeOnline -UserPrincipalName "admin email address"
 
 Add-MailboxPermission -Identity 'Mailbox' -User 'Username' -AccessRights FullAccess -InheritanceType All -Automapping $True
