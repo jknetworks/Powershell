d337cd7cddaf75e42772695126dd28d5528649f8
John Kaiser <67724664+jknetworks@users.noreply.github.com>
2022-09-07 20:46:35 -0700

    Create folderperms.ps1

---
+++ folderperms.ps1
@@ -0,0 +1,15 @@
+$OutFile = "C:\permsfolder\permissions.csv"
+$Header = "Folder Path,IdentityReference,AccessControlType,IsInherited,InheritanceFlags,PropagationFlags"
+Del $OutFile
+Add-Content -Value $Header -Path $OutFile
+
+$RootPath = "\\fs1\shared"
+
+$Folders = dir $RootPath -recurse | where {$_.psiscontainer -eq $true}
+
+foreach ($Folder in $Folders){
+$ACLs = get-acl $Folder.fullname | ForEach-Object { $_.Access }
+Foreach ($ACL in $ACLs){
+$OutInfo = $Folder.Fullname + "," + $ACL.IdentityReference + "," + $ACL.AccessControlType + "," + $ACL.IsInherited + "," + $ACL.InheritanceFlags + "," + $ACL.PropagationFlags
+Add-Content -Value $OutInfo -Path $OutFile
+}}
