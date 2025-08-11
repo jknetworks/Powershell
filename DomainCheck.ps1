e2f3a86f62dfdb9e8e8f13036b5be4bd6edce501
John Kaiser <67724664+jknetworks@users.noreply.github.com>
2025-08-11 16:14:33 -0700

    Add files via upload

---
+++ DomainCheck.ps1
@@ -0,0 +1,10 @@
+﻿# Check if the computer is joined to a domain or workgroup
+
+# Get computer system information
+$compSys = Get-CimInstance Win32_ComputerSystem
+
+if ($compSys.PartOfDomain) {
+    Write-Host "This computer is joined to the domain: $($compSys.Domain)" -ForegroundColor Green
+} else {
+    Write-Host "This computer is part of a workgroup: $($compSys.Workgroup)" -ForegroundColor Yellow
+}
